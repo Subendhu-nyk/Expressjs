4d3de46dd72b3095a91270e136612deccc5ca92e
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-05-16 08:08:42 +0530

    Routing

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
+// const express = require('express');
+// const app = express();
+
+// const bodyParser=require('body-parser')
+// app.use(bodyParser.urlencoded({extended:false}));
+
+// app.use('/add-product',(req, res, next) => {
+//     res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size_product"><button type="submit">Add Product</button></form>');
+//    });
+
+// app.post('/product',(req, res, next) => {
+//     console.log(req.body);    
+//     res.redirect('/')
+// });
+
+// app.use('/', (req, res, next) => {
+//     res.send('<h1>Hello from Express!</h1>');
+//   });
+
+// app.listen(3000);
+
+// task 3
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const bodyParser=require('body-parser')
-app.use(bodyParser.urlencoded({extended:false}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size_product"><button type="submit">Add Product</button></form>');
-   });
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/product',(req, res, next) => {
-    console.log(req.body);    
-    res.redirect('/')
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-  });
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
