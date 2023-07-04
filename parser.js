7646dee5db1dfdfd812c2be5a900ec4b08027b44
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-07-04 11:51:43 +0530

    Intro to Middlewares(task 2)

---
+++ parser.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const server=express()
+const bodyParser=require('body-parser')
+server.use(bodyParser.urlencoded({extended:false}))
+server.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><input type="number" name="size" /><button type="submit">Save</button></form>')
+  
+})
+server.post('/product',(req,res,next)=>{
+    console.log(JSON.stringify(req.body));
+    res.redirect('/')
+})
+server.use('/',(req,res,next)=>{
+    res.send('<h1> Hello from Express </h1>')
+})
+
+server.listen(3200)
