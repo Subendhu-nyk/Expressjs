2ab2d069df1cc10b40e2f624551cbfed460c8ea0
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-07-10 08:45:23 +0530

    Sequelize

--- Edit and delete/util/database.js
+++ Edit and delete/util/database.js
@@ -1,10 +1,21 @@
-const mysql= require('mysql2')
+// const mysql= require('mysql2')
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node_complete',
-    password:'Saibaba@1234'
+// const pool=mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node_complete',
+//     password:'Saibaba@1234'
+// });
+
+// module.exports=pool.promise();
+
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node_complete', 'root', 'Saibaba@1234', 
+{
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports=pool.promise();
+module.exports = sequelize;
