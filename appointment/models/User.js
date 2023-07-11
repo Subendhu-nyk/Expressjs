dcc283b550c22ddfddc353f1e00233ac2949c821
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-07-11 11:48:32 +0530

    appointment

---
+++ appointment/models/User.js
@@ -0,0 +1,37 @@
+
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+
+
+const User = sequelize.define('User', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      }, 
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  date: {
+    type: Sequelize.DATEONLY,
+    allowNull: false,
+  },
+  time: {
+    type: Sequelize.TIME,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
